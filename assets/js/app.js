cc3d04558bc72cc614588a190114791118b271ff
Napatchol Thaipanich <patorseing@gmail.com>
2023-01-03 00:40:11 +0700

    chore: try gauss_elimination

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,9 @@
-import "../ts/main";
-
 import "phoenix_html";
 
 import { Socket } from "phoenix";
 import { LiveSocket } from "phoenix_live_view";
 import topbar from "../vendor/topbar";
+import "../ts/main";
 
 let csrfToken = document
   .querySelector("meta[name='csrf-token']")
--- lib/lights_out_game_web/live/board.ex
+++ lib/lights_out_game_web/live/board.ex
@@ -5,7 +5,11 @@ defmodule LightsOutGameWeb.Board do
   def mount(_params, _session, socket) do
     grid = for x <- 0..4, y <- 0..4, into: %{}, do: {{x, y}, false}
 
-    {:ok, assign(socket, grid: grid, count: 0, win: false)}
+    coefficient = get_coefficient()
+    pivot = get_pivot(grid)
+
+    {:ok,
+     assign(socket, grid: grid, count: 0, win: false, coefficient: coefficient, pivot: pivot)}
   end
 
   def handle_event("start", %{}, socket) do
@@ -21,12 +25,19 @@ defmodule LightsOutGameWeb.Board do
 
     # grid = Map.merge(grid, level_ransom)
 
+    coefficient = get_coefficient()
+    pivot = get_pivot(grid)
+    gauss_elimination(coefficient, pivot)
+
     # updated_grid = Enum.reduce(grid, fn point, acc -> Map.put(acc, point, !grid[point]) end)
-    {:noreply, assign(socket, grid: grid, count: 0, win: false)}
+    {:noreply,
+     assign(socket, grid: grid, count: 0, win: false, coefficient: coefficient, pivot: pivot)}
   end
 
   def handle_event("toggle", %{"x" => strX, "y" => strY}, socket) do
     grid = socket.assigns.grid
+    coefficient = socket.assigns.coefficient
+    pivot = socket.assigns.pivot
     count = socket.assigns.count + 1
     grid_x = String.to_integer(strX)
     grid_y = String.to_integer(strY)
@@ -37,14 +48,20 @@ defmodule LightsOutGameWeb.Board do
       |> then(fn toggled_grid -> Map.merge(grid, toggled_grid) end)
 
     win = check_win(updated_grid)
-    socket = assign(socket, grid: updated_grid, count: count, win: win)
+
+    socket =
+      assign(socket,
+        grid: updated_grid,
+        count: count,
+        win: win,
+        coefficient: coefficient,
+        pivot: pivot
+      )
 
     case win do
       true -> {:noreply, push_event(socket, "gameover", %{win: win})}
       _ -> {:noreply, socket}
     end
-
-    {:noreply, assign(socket, grid: updated_grid, count: count, win: win)}
   end
 
   defp find_adjacent_tile(x, y) do
@@ -57,27 +74,51 @@ defmodule LightsOutGameWeb.Board do
   end
 
   defp check_win(grid) do
-    grid
-    |> Map.values()
+    get_pivot(grid)
     |> Enum.all?(fn light -> !light end)
   end
 
-  # defp grass(grid) do
-  #   coefficient =
-  #     for i <- 0..4,
-  #         j <- 0..4,
-  #         do: for(x <- 0..4, y <- 0..4, do: Enum.member?(find_adjacent_tile(i, j), {x, y}))
+  defp get_coefficient() do
+    transpose(
+      for i <- 0..4,
+          j <- 0..4,
+          do: for(x <- 0..4, y <- 0..4, do: Enum.member?(find_adjacent_tile(i, j), {x, y}))
+    )
+  end
 
-  #   goal =
-  #     grid
-  #     |> Map.values()
+  def transpose(matrix) do
+    cols = Enum.count(Enum.at(matrix, 0))
 
-  #   # Logger.debug(inspect(coefficient))
+    Enum.map(1..cols, fn c ->
+      Enum.map(matrix, fn row -> Enum.at(row, c - 1) end)
+    end)
+  end
 
-  #   # Logger.debug(inspect(grid))
-  # end
+  defp get_pivot(grid) do
+    grid
+    |> Map.values()
+  end
 
-  # defp boolean_to_integer(bool) do
-  #   if bool, do: 1, else: 0
-  # end
+  defp gauss_elimination(a, b) do
+    n = Enum.count(a)
+
+    # Forward elimination
+    for i <- 0..(n - 2) do
+      for j <- (i + 1)..(n - 1) do
+        m = boolean_to_integer(getList(a, j, i)) / boolean_to_integer(getList(a, i, i))
+
+        # for k <- i..(n - 1) do
+        #   getList(a, j, k) = getList(a, j, k) - m * getList(a, i, k)
+        # end
+      end
+    end
+  end
+
+  defp getList(a, x, y) do
+    Enum.at(Enum.at(a, x), y)
+  end
+
+  def boolean_to_integer(bool) do
+    if bool, do: 1, else: 0
+  end
 end
